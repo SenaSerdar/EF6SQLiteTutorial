d0ccbe4a14cdd91c2af9f66dad63de88f73d24d1
sena serdar <senaserdar0093@gmail.com>
2023-01-05 16:04:55 +0300

    EF6SQLite is created

---
+++ EF6SQLite/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EF6SQLite.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
