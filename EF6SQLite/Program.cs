df04b4e9bdbb7b5369fc10d489a68cdabe7fa065
sena serdar <senaserdar0093@gmail.com>
2023-01-05 17:31:11 +0300

    run dotnet ef migrations add Initial

--- EF6SQLite/Program.cs
+++ EF6SQLite/Program.cs
@@ -1,10 +1,13 @@
+using EF6SQLite.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
+builder.Services.AddDbContext<DataContext>(options=>options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"),b=>b.MigrationsAssembly("EF6SQLite")));
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
